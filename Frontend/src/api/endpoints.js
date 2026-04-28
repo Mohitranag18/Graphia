b3b5910f5f636365a89d983b851d1b9ce3a3ec38
Mohit Rana <mohitr8998@gmail.com>
2026-04-29 01:59:17 +0530

    fix prod bug

--- Frontend/src/api/endpoints.js
+++ Frontend/src/api/endpoints.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:8000/api/'
-export const SERVER_URL = 'http://127.0.0.1:8000'
+const BASE_URL = `${import.meta.env.VITE_BASE_URL || 'http://127.0.0.1:8000'}/api/`
+export const SERVER_URL = import.meta.env.VITE_BASE_URL || 'http://127.0.0.1:8000'
 const LOGIN_URL = `${BASE_URL}token/`
 const REFRESH_URL = `${BASE_URL}token/refresh/`
 const NOTES_URL = `${BASE_URL}notes/`
